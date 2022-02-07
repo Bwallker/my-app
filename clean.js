3fa3f29e6e6e294ea018d07cbec37b61aa8aac74
Bwallker <benbwall@gmail.com>
2022-02-07 16:29:56 +0200

    More formatting

--- clean.js
+++ clean.js
@@ -1,7 +1,11 @@
 import { rmSync, mkdirSync } from 'fs';
+
 console.log('Running clean script');
+
 console.log('Removing old pre pre-build-target');
 rmSync('./pre-build-target', { force: true, recursive: true });
+
 console.log('Creating new pre-build-target');
 mkdirSync('pre-build-target');
+
 console.log('clean script finished.');
